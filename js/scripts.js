0fcfec7628911b7e381521eecca4123caa5e6f30
Dolphine Atieno Awuor <awuordolphine65@gmail.com>
2020-02-15 13:47:03 +0300

    add java script codes

--- js/scripts.js
+++ js/scripts.js
@@ -1,138 +1,37 @@
-var century, year, month, dayOfMonth, dayOfWeek, day;
-//Get input
-function getInput(){
-    century = parseInt(document.getElementById("century").value);
+var maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var daysOfWeek=["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+
+
+
+// calling function
+  function getName(){
   year = parseInt(document.getElementById("year").value);
   month = parseInt(document.getElementById("month").value);
-  dayOfMonth = parseInt(document.getElementById("monthday").value);
+  day = parseInt(document.getElementById("day").value);
+  male=document.getElementById("male");
+  female=document.getElementById("female");
 
-    if(century == ""){
-        alert("Input the correct gender");
-        return false;
-        }
-    else if (year == ""){
-        alert("Input the correct year");
-        return false;
-        }
-    else if (month == ""){
-        alert("Input the correct month");
-        return false;
-        }
-    else if(dayOfMonth == ""){
-        alert("input the correct date");
-        return false;
-        }
+// validation
+
+if(day<=0||day>31)
+alert("Oops!please enter a valid day");
+else if(month<0||month>12 ||month ==2 && day>29)
+alert("Oops!please enter a valid month");
+
+
+var day = new Date(year + "/" + month + "/" + day);
+
+var birthDay=day.getDay();
+
+if(male.checked==true){
+   alert("You were born on " +daysOfWeek[birthDay]+ " and your akan name is " + maleNames[birthDay])
+}
+else if(female.checked==true){
+   alert("You were born on " +daysOfWeek[birthDay]+ " and your akan name is " + femaleNames[birthDay])
 }
 
-//Calculate func
-function calculateDay(){
-    getInput();
-    dayOfWeek = ((((century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + dayOfMonth) % 7) -1;
-    console.log(dayOfWeek); //Test the calculateDay function
-    return (Math.floor(dayOfWeek));
-    if (dayOfWeek < 0) {
-        dayOfWeek = dayOfWeek * -1;
-        }
-    else if (dayOfWeek > 0) {
-        return dayOfWeek;
-        }
 }
-
-//main caller func
-function checkDayOfWeek(){
-    day = calculateDay();
-     checkGender();
-     console.log("The function runs");//Test chackDayOfWeek function
-}
-
-//arrays
-let daysOfWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-
-//get selected radio button
-function checkGender(){
-    var gen = document.getElementsByName("rads");
-    if(gen[0].checked == true){
-        var gender = "male";
-        }
-    else if(gen[1].checked == true){
-        var gender = "female";
-        }
-    else {
-      console.log("pass");//Test the radio buttons
-        }
-
-switch(gender){
-    case gender = "male":
-        switch(day){
-            case (0 || -0):
-                document.getElementById("result").innerHTML = "The day is on a sunday." + "  " + "Your akan name is " + maleNames[0];
-                break;
-                case (1 || -1):
-                  document.getElementById("result").innerHTML = "The day is on a monday." + " " + "Your akan name is " + maleNames[1];
-                break;
-                case (2 || -2):
-                  document.getElementById("result").innerHTML = "The day is on a tuesday." + " " + "Your akan name is " + maleNames[2];
-                break;
-                case (3 || -3):
-                  document.getElementById("result").innerHTML = "The day is on a wednesday." + " " + "Your akan name is "+ maleNames[3];
-                break;
-                case (4 || -4):
-                  document.getElementById("result").innerHTML = "The day is on a thursday." + " " + "Your akan name is " + maleNames[4];
-                break;
-                case (5 || -5):
-                  document.getElementById("result").innerHTML = "The day is on a friday." + " " + "Your akan name is " + maleNames[5];
-                break;
-                case (6 || -6):
-                  document.getElementById("result").innerHTML = "The day is on a saturday." + " " + "Your akan name is " + maleNames[6];
-                break;
-                default:
-                // console.console.log("Pass");//Test male c
-            } 
-        break;
-        case gender = "female":
-                switch(day){
-                    case 0 || -0:
-                        document.getElementById("result").innerHTML = "The day is on a sunday." + "  " + "Your akan name is  akosua";
-                      break;
-                      case 1 || -1:
-                        document.getElementById("result").innerHTML = "The day is on a monday." + " " + "Your akan name is adwoa ";
-                      break;
-                      case 2 || -2:
-                        document.getElementById("result").innerHTML = "The day is on a tuesday." + " " + "Your akan name is abenaa";
-                      break;
-                      case 3 || -3:
-                        document.getElementById("result").innerHTML = "The day is on a wednesday." + " " + "Your akan name is akua";
-                      break;
-                      case 4 || -4:
-                        document.getElementById("result").innerHTML = "The day is on a thursday." + " " + "Your akan name is yaa";
-                      break;
-                      case 5 || -5:
-                        document.getElementById("result").innerHTML = "The day is on a friday." + " " + "Your akan name is afua";
-                      break;
-                      case 6 || -6:
-                        document.getElementById("result").innerHTML = "The day is on a saturday." + " " + "Your akan name is ama";
-                      break;
-                    }
-
-        break
-        default:
-        console.log("pass");//Test gender switch
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
